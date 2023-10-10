9907a35fe9fcf48f963204246d6366222e4c7fcc
Mirek Madej <m.madej@zsp3zamosc.pl>
2023-10-10 12:34:15 +0200

    Dodaj pliki projektów.

---
+++ 4M_06_SharedResources/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _4M_06_SharedResources
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ 4M_06_SharedResources/Resources/SharedResources.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4M_06_SharedResources.Resources
+{
+    static class SharedResources
+    {
+        public static readonly Color fontColor = Color.FromRgb(0, 255, 0);
+    }
+}
